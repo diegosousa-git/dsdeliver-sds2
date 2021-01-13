b31067a2362e48a6e1cc73a7fc5cb7c9578a47cf
unknown <sousa.s.diego@gmail.com>
2021-01-13 17:06:26 -0300

    arrumando

--- front-mobile/src/types.ts
+++ front-mobile/src/types.ts
@@ -8,11 +8,3 @@ export type Order = {
     products: Product[];
     total: number;
 }
-
-export type Product = {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    imageUri: string;
-}
